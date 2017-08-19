76d1a1f311545add3cc8ffabb5fa093010dc1bec
SURENDER KUMAR <surender7790@gmail.com>
2017-08-19 18:52:06 +0530

    employee ui is added at /hello

--- employee/views.py
+++ employee/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
 
 
 def login(request):
-    return HttpResponse('Hello!')
+    return render(request, "employee.html")
