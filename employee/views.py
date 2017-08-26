707112e9525ed70eb52e26aa91105f5e870a41c8
Sameer Kumar <sameer27995@gmail.com>
2017-08-26 18:55:51 +0530

    employee: Add templates

--- employee/views.py
+++ employee/views.py
@@ -1,9 +1,26 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from employee.models import Employee
 
-
-# Create your views here.
+def school(request):
+    if request.method == 'POST' :
+        return redirect('employee/index.html',{'error_school':"Error"})
+    return render(request, 'employee/index.html', {})
 
 
 def login(request):
-    return render(request, "employee.html")
+    if request.method == 'GET' :
+        return render(request, 'employee/index.html', {})
+    dept = request.POST['school']
+    school_name = ["usict", "usbt", "usct"]
+    if dept not in school_name:
+        return render(request,'employee/index.html', {'error_school':"Error"})
+    else :
+        list = Employee.objects.filter(school=dept)
+        return render(request, 'employee/login.html', {
+            'dept':dept,
+            'list' : list,
+        })
+
+
+def dashboard(request):
+    return render(request, 'employee/dashboard.html', {})
