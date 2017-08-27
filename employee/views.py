613f3508f66967902257c0c883ca1f95a30f0afb
Hemang Kumar <hemangsk@gmail.com>
2017-08-27 12:00:56 +0530

    cors: Improve cors

--- employee/views.py
+++ employee/views.py
@@ -1,5 +1,16 @@
 from django.http import JsonResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
+from django.views.decorators.http import require_http_methods
+
 from employee.models import Employee
 
 
+@require_http_methods(["GET", "POST"])
+def schools(request):
+    schools = [
+        'usict',
+        'uslls',
+        'usct'
+    ]
+
+    return JsonResponse(schools, safe=False)
