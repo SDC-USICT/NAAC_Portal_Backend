2426addf0b07bcd276bcfd2eaf98a5eed6b59045
Hemang Kumar <hemangsk@gmail.com>
2017-08-31 00:12:35 +0530

    Update models.py

--- workshop/models.py
+++ workshop/models.py
@@ -6,10 +6,10 @@ from employee.models import Employee
 
 class Workshop(models.Model):
     employee_id = models.ForeignKey(to=Employee)
-    name = models.CharField(verbose_name = Name,max_length=100)
-    date = models.DateField(verbose_name = Date)
-    duration = models.CharField(verbose_name = Duration(in days),max_length=100)
-    organization = models.CharField(verbose_name = Organization,max_length=100)
+    name = models.CharField(verbose_name = "Name",max_length=100)
+    date = models.DateField(verbose_name = "Date")
+    duration = models.CharField(verbose_name = "Duration(in days)",max_length=100)
+    organization = models.CharField(verbose_name = "Organization",max_length=100)
 
     class Meta:
         db_table = 'workshop'
