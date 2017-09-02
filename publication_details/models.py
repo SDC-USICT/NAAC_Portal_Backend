c94140d9dd34df0a53b68f4a6e08dcf495ee5530
Hemang Kumar <hemangsk@gmail.com>
2017-09-02 12:33:05 +0530

    models: Update publication_details fields

--- publication_details/models.py
+++ publication_details/models.py
@@ -6,7 +6,7 @@ from employee.models import Employee
 
 class JournalPapers(models.Model):
     employee = models.ForeignKey(Employee, related_name='employee_jp', verbose_name='Employee ID')
-    title_of_paper = models.CharField("Paper Title",max_length=100)
+    title = models.CharField("Title of Paper",max_length=100, blank=True, null=True)
     name_and_publisher = models.CharField("Journal Name with Publisher",max_length=100)
     volume_no = models.CharField("Vol. No.",max_length=100)
     issn_isbn = models.CharField("ISSN No.",max_length=100)
@@ -21,7 +21,7 @@ class JournalPapers(models.Model):
 
 class Conference(models.Model):
     employee = models.ForeignKey(Employee, related_name='employee_conf', verbose_name='Employee ID')
-    title_of_paper = models.CharField("Paper Title",max_length=100)
+    title = models.CharField("Title of Paper",max_length=100, blank=True, null=True)
     name_and_publisher = models.CharField('Name & Publisher', max_length=100)
     volume_no = models.CharField('Vol no', max_length=100)
     issn_isbn = models.CharField("ISBN No.",max_length=100)
@@ -37,7 +37,7 @@ class Conference(models.Model):
 
 class BookChapters(models.Model):
     employee = models.ForeignKey(Employee, related_name='employee_book_chapter')
-    title_of_paper = models.CharField("Paper Title",max_length=100)
+    title = models.CharField("Title of Book Chapter",max_length=100, blank=True,null=True)
     book_title_and_publisher = models.CharField("Book Title with Publisher",max_length=100)
     page_no = models.CharField("Page No.",max_length=100)
     isbn = models.CharField("ISBN No.",max_length=100)
@@ -53,7 +53,7 @@ class BookChapters(models.Model):
 
 class Book(models.Model):
     employee = models.ForeignKey(Employee, related_name='employee_book', verbose_name='Employee ID')
-    title_of_book = models.CharField("Book Title",max_length=100)
+    title = models.CharField("Book Title",max_length=100, blank=True, null=True)
     isbn = models.CharField("ISBN",max_length=100)
     year = models.IntegerField("Year")
     author = models.ManyToManyField(to=Employee, related_name='employee_book_author', verbose_name='Authors')
