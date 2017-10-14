72616726263a0fefbfd7b0df82c079cd5caa33c9
Hemang Kumar <hemangsk@gmail.com>
2017-10-14 19:50:40 +0530

    Fix form submission

--- employee/mm_handler.py
+++ employee/mm_handler.py
@@ -2,7 +2,7 @@
 from django.core.mail import EmailMessage
 
 from naac_portal.settings import FRONTEND
-from publication_details.models import Book
+from publication_details.models import *
 
 
 def handler(kls, data):
@@ -11,14 +11,22 @@ def handler(kls, data):
 
     for d in data:
         print(d)
-        for sd in d['coauthor'].split(';'):
-            if sd.split(':')[2] == '0':
-                from employee.models import Employee
-                e = Employee.objects.get(instructor_id=d['employee'])
-                print('mail ' + sd.split(':')[1])
-                stremail = FRONTEND + '/#/approve?id=' + str(d['pk']) + '&title=' + \
-                           d['title'] +'&email=' + sd.split(':')[1] + '&type=' +  kls + \
-                '&invitee=' + (e.name).replace(' ', '')
-                print(stremail)
-                mail = EmailMessage('NAAC Portal CoAuthor Verification Mail', stremail, to=[sd.split(':')[1]])
-                mail.send()
+        if d['coauthor']:
+            for sd in d['coauthor'].split(';'):
+                if sd.split(':')[2] == '0':
+                    from employee.models import Employee
+                    e = Employee.objects.get(instructor_id=d['employee'])
+                    print('mail ' + sd.split(':')[1])
+                    search = Employee.objects.filter(email=sd.split(':')[1])
+                    if search.exists():
+                        stremail = FRONTEND + '/#/approve?id=' + str(d['pk']) + '&title=' + \
+                                   d['title'] +'&email=' + sd.split(':')[1] + '&type=' +  kls + \
+                        '&invitee=' + (e.name).replace(' ', '')
+                        print(stremail)
+                        mail = EmailMessage('NAAC Portal CoAuthor Verification Mail', stremail, to=[sd.split(':')[1]])
+                        try:
+                            mail.send()
+                        except Exception:
+                            print('Mail Fault')
+                    else:
+                        print('Employee not exists in table')
