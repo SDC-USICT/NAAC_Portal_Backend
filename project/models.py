7e2613db2679d08e0c89910b697845bd363bcd42
Hemang Kumar <hemangsk@gmail.com>
2017-10-25 19:06:23 +0530

    Projects: Add new Co Principle investigator

--- project/models.py
+++ project/models.py
@@ -7,12 +7,14 @@ from employee.models import Employee
 class Projects(models.Model):
     employee = models.ForeignKey(to=Employee, related_name='employee_projects', verbose_name='Employee ID')
     title = models.CharField("Project Title",max_length=100)
-    author = models.ManyToManyField(to=Employee, related_name='employee_copi', verbose_name='Co Authors')
     sponsors = models.CharField("Sponsoring Agency",max_length=100)
     date_of_award = models.TextField("Date of Award")
     date_completed = models.TextField("Date of Completed")
     amnt_sanctioned = models.CharField("Amount Sanctioned",max_length=100)
     status = models.CharField("Status",max_length=100)
+    author = models.CharField(max_length=200, verbose_name='Principle Investigator')
+    copi = models.TextField(max_length=200, verbose_name='Co Principle Investigators', blank=True,null=True)
+
 
     class Meta:
         db_table = 'project'
