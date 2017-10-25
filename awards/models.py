fd5d1fe2f7eff2919863fea98450522e2500b2a4
Surender Kumar <surender7790@gmail.com>
2017-10-25 22:03:30 +0530

    models of professional and award change for interfields

--- awards/models.py
+++ awards/models.py
@@ -7,6 +7,7 @@ from employee.models import Employee
 class Awards(models.Model):
     employee = models.ForeignKey(to=Employee, related_name='employee_awards', verbose_name='Employee ID')
     title = models.CharField("Title",max_length=100)
+    awardType = models.CharField("awardType",max_length=100)
     organisation = models.CharField("Organization",max_length=100)
     month = models.CharField("Month",max_length=100)
     year = models.IntegerField("Year")
