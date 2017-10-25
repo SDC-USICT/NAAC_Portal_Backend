34913d20eff8c272f27987c70a26ff9c6dd29b76
Hemang Kumar <hemangsk@gmail.com>
2017-10-25 18:47:50 +0530

    membership: Add new fields

--- moab/models.py
+++ moab/models.py
@@ -9,6 +9,9 @@ class Membership(models.Model):
     title = models.CharField("Membership Type",max_length=100)
     academic_body = models.CharField("Academic Body Name",max_length=100)
     university_agency = models.CharField("University Agency",max_length=100)
+    start_year = models.TextField("Start Year", max_length="10", blank=True, null=True)
+    end_year = models.TextField("End Year", max_length="10", blank=True, null=True)
+
 
     class Meta:
         db_table = 'membership'
