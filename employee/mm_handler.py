5cd4419fcbfd9fbe618c1d7c86dabd2ae752e8c3
Hemang Kumar <hemangsk@gmail.com>
2017-10-25 18:28:29 +0530

    views.py: Fix form submission approach

--- employee/mm_handler.py
+++ employee/mm_handler.py
@@ -7,22 +7,19 @@ from publication_details.models import *
 
 def handler(kls, data):
     klass = eval(kls)
-    print(data)
 
     for d in data:
-        print(d)
         if d['coauthor']:
             for sd in d['coauthor'].split(';'):
                 if sd.split(':')[2] == '0':
                     from employee.models import Employee
                     e = Employee.objects.get(instructor_id=d['employee'])
-                    print('mail ' + sd.split(':')[1])
                     search = Employee.objects.filter(email=sd.split(':')[1])
+                    print(sd.split(':')[1])
                     if search.exists():
                         stremail = FRONTEND + '/#/approve?id=' + str(d['pk']) + '&title=' + \
                                    d['title'] +'&email=' + sd.split(':')[1] + '&type=' +  kls + \
                         '&invitee=' + (e.name).replace(' ', '')
-                        print(stremail)
                         mail = EmailMessage('NAAC Portal CoAuthor Verification Mail', stremail, to=[sd.split(':')[1]])
                         try:
                             mail.send()
