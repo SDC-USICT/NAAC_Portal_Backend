c74a3ddebf3116a4d60c400b553eb1db9ee4e394
Surender Kumar <surender7790@gmail.com>
2017-10-26 00:35:06 +0530

    changes for form fields type

--- publication_details/models.py
+++ publication_details/models.py
@@ -26,9 +26,9 @@ class Conference(models.Model):
     title = models.CharField("Title of Paper",max_length=100, blank=True, null=True)
     name_and_publisher = models.CharField('Name & Publisher', max_length=100)
     volume_no = models.CharField('Vol no', max_length=100)
-    issn_isbn = models.CharField("ISBN No.",max_length=100)
+    issn_isbn = models.IntegerField("ISBN No.")
     indexing = models.CharField("Indexing",max_length=100)
-    year = models.IntegerField("Year")
+    year = models.CharField("Year",max_length=4)
     month = models.CharField("Month",max_length=12)
     international_national = models.CharField("International/National",max_length=100)
     coauthor = models.TextField(max_length=200, verbose_name='Co Authors',blank=True,null=True)
@@ -41,10 +41,10 @@ class BookChapters(models.Model):
     employee = models.ForeignKey(Employee, related_name='employee_book_chapter')
     title = models.CharField("Title of Book Chapter",max_length=100, blank=True,null=True)
     book_title_and_publisher = models.CharField("Book Title with Publisher",max_length=100)
-    page_no = models.CharField("Page No.",max_length=100)
-    isbn = models.CharField("ISBN No.",max_length=100)
+    page_no = models.IntegerField("Page No.")
+    isbn = models.IntegerField("ISBN No.")
     indexing = models.CharField("Indexing",max_length=100)
-    year = models.IntegerField("Year")
+    year = models.CharField("Year",max_length=4)
     month = models.CharField("Month",max_length=12)
     coauthor = models.TextField(max_length=200, verbose_name='Co Authors',blank=True,null=True)
 
@@ -56,8 +56,8 @@ class BookChapters(models.Model):
 class Book(models.Model):
     employee = models.ForeignKey(Employee, related_name='employee_book')
     title = models.CharField(max_length=100, verbose_name='Title of Book', blank=True, null=True)
-    isbn = models.CharField(max_length=100, verbose_name='ISBN', blank=True, null=True)
-    year = models.IntegerField("Year")
+    isbn = models.IntegerField('ISBN')
+    year = models.CharField("Year",max_length=4)
     coauthor = models.TextField(max_length=200, verbose_name='Co Authors',blank=True,null=True)
 
     class Meta:
