ebb7b70e6596e10a27d53cf41efce407fa4b2a4a
Surender Kumar <surender7790@gmail.com>
2017-10-26 00:35:06 +0530

    changes for form fields type

--- project/models.py
+++ project/models.py
@@ -7,10 +7,10 @@ from employee.models import Employee
 class Projects(models.Model):
     employee = models.ForeignKey(to=Employee, related_name='employee_projects', verbose_name='Employee ID')
     title = models.CharField("Project Title",max_length=100)
-    sponsors = models.CharField("Sponsoring Agency",max_length=100)
+    sponsors = models.TextField("Sponsoring Agency")
     date_of_award = models.TextField("Date of Award")
     date_completed = models.TextField("Date of Completed")
-    amnt_sanctioned = models.CharField("Amount Sanctioned",max_length=100)
+    amnt_sanctioned = models.IntegerField("Amount Sanctioned")
     status = models.CharField("Status",max_length=100)
     author = models.CharField(max_length=200, verbose_name='Principle Investigator', blank=True, null=True)
     coauthor = models.TextField(max_length=200, verbose_name='Co Principle Investigators', blank=True,null=True)
