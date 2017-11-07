8458d6352590230a8f200a53c8f87ce5a6c43aa8
Hemang Kumar <hemangsk@gmail.com>
2017-11-07 12:34:35 +0530

    naac_portal: Fix patent model

--- patents/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Patents
-
-
-class PatentForm(forms.ModelForm):
-    class Meta:
-        model = Patents
-        fields = ('name', 'patenting_agency', 'year_application', 'year_grant')
