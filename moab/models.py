682648961ed9613264919a60e37c7d04be692548
Surender Kumar <surender7790@gmail.com>
2017-11-07 20:43:34 +0530

    modified for changes on nov 7

--- moab/models.py
+++ moab/models.py
@@ -8,7 +8,7 @@ class Membership(models.Model):
     employee = models.ForeignKey(to=Employee, related_name='employee_moab', verbose_name='Employee ID')
     title = models.CharField("Membership Type",max_length=100)
     academic_body = models.CharField("Academic Body Name",max_length=100)
-    university_agency = models.CharField("University Agency",max_length=100)
+    type = models.CharField("Membership Type",max_length=100)
     start_year = models.CharField("Start Year", max_length=4)
     end_year = models.CharField("End Year", max_length=4)
 
--- professional_details/models.py
+++ professional_details/models.py
@@ -7,14 +7,14 @@ from employee.models import Employee
 
 class Professional(models.Model):
     employee = models.ForeignKey(to=Employee, verbose_name='Employee ID')
-    title = models.IntegerField("Academic Experience")
-    industrial_exp = models.IntegerField("Industrial Experience")
+    title = models.CharField("Academic Experience",max_length=100)
+    industrial_exp = models.CharField("Industrial Experience",max_length=100)
     qualification_highest = models.CharField("Highest Qualification",max_length=100)
     qualification_year_completion = models.CharField("Year Of Completion of Highest Qualification",max_length=100)
     phds = models.IntegerField("Total Student")
-    pursuing = models.CharField("Phd Pursuing",max_length=100)
-    submitted = models.CharField("Phd Submitted", max_length=100)
-    awarded = models.CharField("Phd Awarded",max_length=100)
+    pursuing = models.IntegerField("Phd Pursuing")
+    submitted = models.IntegerField("Phd Submitted")
+    awarded = models.IntegerField("Phd Awarded")
     year = models.CharField("Year",max_length=4, default=(datetime.datetime.now()).year)
 
     class Meta:
