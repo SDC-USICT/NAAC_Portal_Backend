29ca12b6e5fc398d1cbdfb92c0847c3f37d2ca4b
Surender Kumar <surender7790@gmail.com>
2017-11-10 16:36:31 +0530

    workshop: new field role

--- workshop/models.py
+++ workshop/models.py
@@ -7,6 +7,7 @@ from employee.models import Employee
 class Workshop(models.Model):
     employee = models.ForeignKey(to=Employee, verbose_name='Employee ID')
     title = models.CharField(verbose_name = "Name",max_length=100)
+    role = models.CharField(verbose_name = "Role",max_length=100)
     date = models.TextField(verbose_name = "Date", blank=True, null=True)
     duration = models.CharField(verbose_name = "Duration(in days)",max_length=100)
     organization = models.CharField(verbose_name = "Organization",max_length=100)
