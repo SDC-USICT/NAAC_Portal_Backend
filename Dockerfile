55fdf2dedb7dd1749ae0bbcb70689487b7614e39
Hemang Kumar <hemangsk@gmail.com>
2018-02-15 14:57:11 +0530

    Remove unneeded static files

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,5 @@ EXPOSE 80
 RUN pip3 install -r requirements.txt
 RUN python3 manage.py makemigrations
 RUN python3 manage.py migrate
-RUN python3 manage.py collectstatic --noinput
 
 ENTRYPOINT python3 manage.py runserver 0.0.0.0:80
--- naac_portal/settings.py
+++ naac_portal/settings.py
@@ -156,9 +156,9 @@ STATICFILES_FINDERS = [
 ]
 
 STATICFILES_DIRS = (
-  os.path.join(BASE_DIR, 'static/'),
+  os.path.join(BASE_DIR, 'staticfiles/'),
 )
-STATIC_ROOT = os.path.join(BASE_DIR,  'static')
+STATIC_ROOT =os.path.normpath(os.path.join(BASE_DIR, 'static'))
 STATIC_URL =  '/static/'
 
 
